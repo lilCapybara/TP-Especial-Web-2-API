7d13fc4e3a0d0ea3b41ea9de246a7ad728e2d180
Sebastián cisneros <134341014+roquefortman@users.noreply.github.com>
2023-11-12 22:09:35 -0300

    filter,order,pagination
    ahi debe estar

--- TPE apirest/app/controllers/api.campeones.php
+++ TPE apirest/app/controllers/api.campeones.php
@@ -12,29 +12,23 @@ class ApiCampeones extends ApiController {
     }
 
     function get($params = []) {
-        $id = $params[":Champion_id"];
-    
-        if (empty($id)) {
-            $champ = $this->model->getChampOrdeXNombre();
-            $this->view->response([
-                'data' => $champ,
-                'status' => "success"
-            ], 200);
-        } else {
-            $champ = $this->model->getChampById($id);
-    
-            if (!empty($champ)) {
-                $this->view->response([
-                    'data' => $champ,
-                    'status' => 'success',
-                ], 200);
-            } else {
-                $this->view->response([
-                    'data' => 'El campeón solicitado no existe',
-                    'status' => 'error'
-                ], 404);
-            }
-        }
+        $columns = $this->model->getColumnNames();
+
+        // Arreglo donde se almacenarán los parámetros de consulta
+        $queryParams = array();
+
+        // Filtro
+        $queryParams += $this->handleFilter($columns);
+
+        // Ordenamiento
+        $queryParams += $this->handleSort($columns);
+
+        // Paginación
+        $queryParams += $this->handlePagination();
+
+        // Se obtienen los álbumes y se devuelven en formato JSON
+        $albums = $this->model->getAllChamp($queryParams);
+        return $this->view->response($albums, 200);
     }
 
     function delete($params = []) {
@@ -92,6 +86,106 @@ class ApiCampeones extends ApiController {
             } else {
                 $this->view->response('El campeon con id='.$Champion_id.' no existe.', 404);
             }
+        }
+
+        private function handleFilter($columns) {
+            // Valores por defecto
+            $filterData = [
+                'filter' => "", // Campo de filtrado
+                'value' => ""   // Valor de filtrado
+            ];
+    
+            if (!empty($_GET['filter']) && !empty($_GET['value'])) {
+                $filter = $_GET['filter'];
+                $value = $_GET['value'];
+
+              
+    
+                // Si el campo no existe se produce un error
+                if (!in_array($filter, $columns)) {
+                    $this->view->response("Invalid filter parameter (field '$filter' does not exist)", 400);
+                    die();
+                }
+    
+                $filterData['filter'] = $filter;
+                $filterData['value'] = $value;
+           
+            }
+    
+            return $filterData;
+        }
+    
+        /**
+         * Método de ordenamiento de resultados según campo y orden dados
+         */
+        private function handleSort($columns) {
+            // Valores por defecto
+            $sortData = [
+                'sort' => "", // Campo de ordenamiento
+                'order' => "" // Orden ascendente o descendente
+            ];
+    
+            if (!empty($_GET['sort'])) {
+                $sort = $_GET['sort'];
+
+              
+    
+                // Si el campo de ordenamiento no existe se produce un error
+                if (!in_array($sort, $columns)) {
+                    $this->view->response("Invalid sort parameter (field '$sort' does not exist)", 400);
+                    die();
+                }
+    
+                // Orden ascendente o descendente
+                if (!empty($_GET['order'])) {
+                    $order = strtoupper($_GET['order']);
+                    $allowedOrders = ['ASC', 'DESC'];
+    
+                    // Si el campo de ordenamiento no existe se produce un error
+                    if (!in_array($order, $allowedOrders)) {
+                        $this->view->response("Invalid order parameter (only 'ASC' or 'DESC' allowed)", 400);
+                        die();
+                    }
+                }
+    
+                $sortData['sort'] = $sort;
+                $sortData['order'] = $order;
+            }
+    
+            return $sortData;
+        }
+    
+        /**
+         * Método de paginación de resultados según número de página y límite dados
+         */
+        private function handlePagination() {
+            // Valores por defecto
+            $paginationData = [
+                'limit' => 0,    // Límite de resultados
+                'offset' => 0    // Desplazamiento
+            ];
+  
+    
+            if (!empty($_GET['page']) && !empty($_GET['limit'])) {
+                $page = $_GET['page'];
+                $limit = $_GET['limit'];
+
+                
+              
+    
+                // Si alguno de los valores no es un número natural se produce un error
+                if (!is_numeric($page) || $page < 0 || !is_numeric($limit) || $limit < 0) {
+                    $this->view->response("Page and limit parameters must be positive integers", 400);
+                    die();
+                }
+    
+                $offset = ($page - 1) * $limit;
+    
+                $paginationData['limit'] = $limit;
+                $paginationData['offset'] = $offset;
+            }
+           
+            return $paginationData;
         }
 
     }
--- TPE apirest/app/models/campeones.model.php
+++ TPE apirest/app/models/campeones.model.php
@@ -2,12 +2,31 @@
 require_once 'app/models/model.php';
 
 class Campeones_model extends Model {
-    public function getAllChamp() {
-        $query = $this->db->prepare('SELECT * FROM `campeones` ORDER BY nombre ASC ');
+    public function getAllChamp( $queryParams) {
+        $sql = "SELECT * FROM campeones";
+
+        // Filtro
+        if (!empty($queryParams['filter']) && !empty($queryParams['value']))
+            $sql .= ' WHERE ' . $queryParams['filter'] . ' LIKE \'%' . $queryParams['value'] . '%\'';
+
+        // Ordenamiento
+        if (!empty($queryParams['sort'])) {
+            $sql .= ' ORDER BY '. $queryParams['sort'];
+
+            // Orden ascendente y descendente
+            if (!empty($queryParams['order']))
+                $sql .= ' ' . $queryParams['order'];
+        }
+
+        // Paginación
+        if (!empty($queryParams['limit']))
+            $sql .= ' LIMIT ' . $queryParams['limit'] . ' OFFSET ' . $queryParams['offset'];
+
+        // No hace falta sanitizar consulta (datos ingresados ya fueron verificados por el controlador)
+        $query = $this->db->prepare($sql);        
         $query->execute();
 
         $champ = $query->fetchAll(PDO::FETCH_OBJ);
-
         return $champ;
     }
 
@@ -16,6 +35,13 @@ class Campeones_model extends Model {
         $query = $this->db->prepare('UPDATE campeones SET nombre = ?, rol = ?, precio = ? WHERE Champion_id = ?');
         $query->execute([$nombre, $rol, $Precio, $Champion_id]);
     }
+
+    public function getColumnNames() {
+        $query = $this->db->query('DESCRIBE campeones');
+        $columns = $query->fetchAll(PDO::FETCH_COLUMN);
+        return $columns;
+    }
+
 
     public   function getChampById($Champion_id) {
         $query = $this->db->prepare('SELECT * FROM `campeones` WHERE Champion_id=?');
@@ -33,14 +59,14 @@ class Campeones_model extends Model {
         return $this->db->lastInsertId();
     }
 
-    
-    public function getChampOrdeXNombre() {
+    /*
+    public function getChampOrderByName() {
 
-        $query = $this->db->prepare('SELECT * FROM campeones ORDER BY nombre ASC');
-        $query->execute([]);
+        $query = $this->db->prepare('SELECT * ORDER BY nombre ASC');
+        $query->execute();
         $Champ = $query->fetchAll(PDO::FETCH_OBJ);
         return $Champ;
-    }
+    }*/
 
     public function deleteChamp($Champion_id) {
         $query = $this->db->prepare('DELETE FROM campeones WHERE Champion_id =?');
